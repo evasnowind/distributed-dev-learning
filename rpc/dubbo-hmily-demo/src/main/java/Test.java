57dcc6862082f87380de8a119a93b01af6002cfe
Chenglong Yu <yuchenglong8809@163.com>
2020-12-16 23:01:23 +0800

    add dubbo demo(to be continue)

---
+++ rpc/dubbo-hmily-demo/src/main/java/Test.java
@@ -0,0 +1,12 @@
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author chenglong.yu
+ * created on 2020/12/15
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("-" + StandardCharsets.UTF_8.name() + "-");
+    }
+}
